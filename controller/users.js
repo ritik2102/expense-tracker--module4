c47cb044f8f2d01d5c889147ec21d3c7242d14c2
ritik2102 <ritik21feb@gmail.com>
2023-04-23 16:40:27 +0530

    Secured the passwords

--- controller/users.js
+++ controller/users.js
@@ -1,11 +1,14 @@
 const User=require('../model/user');
+const bcrypt=require("bcrypt");
 
 exports.postUser=(req,res,next)=>{
     
     const name=req.body.name;
     const email=req.body.email;
     const password=req.body.password;
-    User.create({name:name,email:email,password:password})
+
+    bcrypt.hash(password,10, (err,hash)=>{
+        User.create({name:name,email:email,password:hash})
         .then(result=>{
             res.status(201).json({resData:"success"});
         })
@@ -14,6 +17,8 @@ exports.postUser=(req,res,next)=>{
             const error=err.errors[0].message;
             res.status(201).json({resData:error});
         })
+    })
+    
 }
 
 exports.postUserLogin=(req,res,next)=>{
@@ -29,14 +34,27 @@ exports.postUserLogin=(req,res,next)=>{
             }
             // user exists
             else{
+                hash=users[0].dataValues.password;
+                console.log(hash);
+                bcrypt.compare(password,users[0].dataValues.password,(err,result)=>{
+                    if(err){
+                        throw new Error("Something went wrong");
+                    }
+                    if(result==true){
+                        res.status(201).json({resData:'loginSuccessful'});
+                    }
+                    else{
+                        res.status(401).json({resData:'incorrectPassword'});
+                    }
+                })
                 // password does not match
-                if(users[0].dataValues.password!==password){
-                    res.status(401).json({resData:'incorrectPassword'});
-                }
-                // password matches
-                else{
-                    res.status(201).json({resData:'loginSuccessful'});
-                }
+                // if(users[0].dataValues.password!==password){
+                //     res.status(401).json({resData:'incorrectPassword'});
+                // }
+                // // password matches
+                // else{
+                //     res.status(201).json({resData:'loginSuccessful'});
+                // }
             }
         })
         .catch(err=>{
---
+++ login.js
@@ -0,0 +1,45 @@
+const emailField=document.getElementById('email');
+const passwordField=document.getElementById('password');
+const loginMessage=document.querySelector('.login-message');
+const passwordMessage=document.querySelector('.password-message');
+
+function init(){
+    loginMessage.style.visibility='hidden';
+    passwordMessage.style.visibility='hidden';
+    emailField.value="";
+    passwordField.value="";
+}
+init();
+
+async function login(e){
+    try{
+        e.preventDefault();
+        // making the messages invisible again
+        loginMessage.style.visibility='hidden';
+        passwordMessage.style.visibility='hidden';
+
+        const email=emailField.value;
+        const password=passwordField.value;
+
+        const credentials={
+            "email":email,
+            "password":password
+        }
+        console.log(credentials);
+        const res=await axios.post('http://localhost:3000/users/user-login',credentials);
+        
+        console.log('Login successful');
+        alert("Successful user login");
+        emailField.value="";
+        passwordField.value="";
+    }
+    catch(err){
+        console.log(err.response.status);
+        if(err.response.status===401){
+            passwordMessage.style.visibility='visible';
+        }
+        else if(err.response.status===404){
+            loginMessage.style.visibility='visible';
+        }
+    }
+}
